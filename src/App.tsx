a6c5a56dd72371478114e013a299e04833f24ef4
john <johndessie77@gmail.com>
2022-11-30 22:35:11 +0300

    Init Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import "./App.css";
+import "./index.css";
 
 function App() {
+    const form = useForm({
+      initialValues: {
+        name: "",
+        email: "",
+      },
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider>
+      <div>
+        Hello World!
+      </div>
+    </MantineProvider>
   );
 }
 
